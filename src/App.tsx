80a2d01afd886ab473c965cec91786e6c2275f1e
Ojonugwa Martins Onogu <onogumartins01@gmail.com>
2023-06-13 20:10:38 +0100

    working on home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar";
 import { SelectedPage } from "@/shared/types";
-import Home from "@/scenes/navbar";
+import Home from "@/scenes/Home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
